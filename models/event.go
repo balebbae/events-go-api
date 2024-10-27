a0f01f60294b8b1887615d1735a3fd3afd11e17d
Caleb Bae <baecal000@gmail.com>
2024-10-27 16:23:37 -0500

    connect to sqlite3 and write save event

---
+++ models/event.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"time"
+
+	"github.com/balebbae/events-go-api/db"
+)
+
+type Event struct {
+	ID int64 
+	Name string `binding:"required"`
+	Description string `binding:"required"`
+	Location string `binding:"required"`
+	DateTime time.Time `binding:"required"`
+	UserID int
+}
+
+var events []Event = []Event{}
+
+func (e Event) Save() error {
+	query := `INSERT INTO events(name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
+}
+
+func GetAllEvents() ([]Event) {
+	return events
+}
