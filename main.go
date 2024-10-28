d664b7c106a901acc99f0f0096c915461f5179ef
Caleb Bae <baecal000@gmail.com>
2024-10-28 09:10:03 -0500

    refactor create and get events

--- main.go
+++ main.go
@@ -1,49 +1,17 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/balebbae/events-go-api/db"
-	"github.com/balebbae/events-go-api/models"
+	"github.com/balebbae/events-go-api/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
-
-	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
+	
+	routes.RegisterRoutes(server)
 
 	server.Run(":8080") // localhost:8080
-
 }
 
-func getEvents(context *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events."})
-		return
-	}
-	 context.JSON(http.StatusOK, events)
-}
-
-func createEvent(context *gin.Context) {
-	var event models.Event
-	err := context.ShouldBindJSON(&event)
-
-	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse"})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 1
-	err = event.Save()
-	if err != nil {
-		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event."})
-		return
-	}
-	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
-
-}
