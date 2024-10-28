e2892c1bb326c33b75e103452c03212bf7cff454
Caleb Bae <baecal000@gmail.com>
2024-10-28 11:30:24 -0500

    add basic user signup

---
+++ routes/users.go
@@ -0,0 +1,25 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/balebbae/events-go-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse"})
+		return
+	}
+
+	err = user.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not save user"})
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "User created succuessfully"})
+}
