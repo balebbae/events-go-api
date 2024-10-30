4e84958c0d2a99625a12e93a1d71688bd51b131c
Caleb Bae <baecal000@gmail.com>
2024-10-30 11:30:43 -0500

    create jwt auth

--- routes/events.go
+++ routes/events.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/balebbae/events-go-api/models"
+	"github.com/balebbae/events-go-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -36,16 +37,28 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
+	err := utils.VerifyToken(token)
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse"})
 		return
 	}
 
-	event.ID = 1
 	event.UserID = 1
+
 	err = event.Save()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event."})
